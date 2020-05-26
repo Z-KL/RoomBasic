ffe97a6d3544e1cc8a6541e6b428743ef8000135
赵柯亮 <2216889648@qq.com>
2020-05-26 10:02:03 +0800

    200526

---
+++ RoomBasic1/app/src/main/java/com/example/roombasic1/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.roombasic1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    Button buttonInsert,buttonClear;
+    TextView textView;
+    MyAdapter myAdapter1,myAdapter2;
+    RecyclerView recyclerView;
+    WordViewModel wordViewModel;
+    Switch aSwitch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recycleView);
+        myAdapter1 = new MyAdapter(false);
+        myAdapter2 = new MyAdapter(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myAdapter1);
+
+        aSwitch = findViewById(R.id.switch1);
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    recyclerView.setAdapter(myAdapter2);
+                }else {
+                    recyclerView.setAdapter(myAdapter1);
+                }
+            }
+        });
+        textView = findViewById(R.id.textViewNumber);
+        buttonInsert = findViewById(R.id.buttonInsert);
+        buttonClear = findViewById(R.id.buttonClear);
+        wordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
+
+        wordViewModel.getAllWordsLive().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(List<Word> words) {
+                myAdapter1.setAllWords(words);
+                myAdapter2.setAllWords(words);
+                myAdapter1.notifyDataSetChanged();
+                myAdapter2.notifyDataSetChanged();
+
+
+
+//                StringBuilder text = new StringBuilder();
+//                for (int i= 0 ;i<words.size();i++){
+//                    Word word = words.get(i);
+//                    text.append(word.getId()).append(":").append(word.getWord()).append("=").append(word.getChineseMeaning()).append("\n");
+//                }
+//                textView.setText(text.toString());
+            }
+        });
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String[] english = {
+                        "Hello",
+                        "World",
+                        "Android",
+                        "Google",
+                        "Studio",
+                        "Project",
+                        "Database",
+                        "Recycler",
+                        "View",
+                        "String",
+                        "Value",
+                        "Integer"
+                };
+                String[] chinese = {
+                        "你好",
+                        "世界",
+                        "安卓系统",
+                        "谷歌公司",
+                        "工作室",
+                        "项目",
+                        "数据库",
+                        "回收站",
+                        "视图",
+                        "字符串",
+                        "价值",
+                        "整数类型"
+                };
+                for (int i = 0; i< english.length; i++){
+                    wordViewModel.insertWords(new Word(english[i],chinese[i]));
+                }
+            }
+        });
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wordViewModel.deleteAllWords();
+
+            }
+        });
+    }
+
+
+}
